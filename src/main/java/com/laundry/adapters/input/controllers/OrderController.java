7c199f06fc76868093f93f045666926712d91036
Danielle Delfino <dhandelfino@gmail.com>
2026-03-09 00:53:01 -0300

    feat: adicionando controller e usecase

---
+++ src/main/java/com/laundry/adapters/input/controllers/OrderController.java
@@ -0,0 +1,21 @@
+package com.laundry.adapters.input.controllers;
+
+import com.laundry.application.usecases.CreateOrderUsecase;
+import com.laundry.domain.entity.Order;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pedidos")
+public class OrderController {
+
+    private final CreateOrderUsecase createOrderUsecase;
+
+    public OrderController(CreateOrderUsecase createOrderUsecase) {
+        this.createOrderUsecase = createOrderUsecase;
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return createOrderUsecase.execute(order);
+    }
+}
--- src/main/java/com/laundry/application/usecases/CreateOrderUsecase.java
+++ src/main/java/com/laundry/application/usecases/CreateOrderUsecase.java
@@ -1,4 +1,20 @@
 package com.laundry.application.usecases;
 
+import com.laundry.domain.entity.Order;
+import com.laundry.domain.repository.OrderRepository;
+
 public class CreateOrderUsecase {
+
+    private final OrderRepository repository;
+
+    public CreateOrderUsecase(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    public Order execute(Order order) {
+
+        order.setStatus("Recebido");
+
+        return repository.save(order);
+    }
 }
