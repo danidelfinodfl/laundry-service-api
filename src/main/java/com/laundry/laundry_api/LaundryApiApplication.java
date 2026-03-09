5a44eb9af7758c07afd7c9e4c417e18e9e942e2a
Danielle Delfino <dhandelfino@gmail.com>
2026-03-08 23:54:46 -0300

    initial commit

---
+++ src/main/java/com/laundry/laundry_api/LaundryApiApplication.java
@@ -0,0 +1,14 @@
+package com.laundry.laundry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryApiApplication.class, args);
+	}
+
+}
+	
