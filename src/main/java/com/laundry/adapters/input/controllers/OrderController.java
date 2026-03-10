555484cae09c00b6a5cb0bb2e20cbbb36762ece7
Danielle Delfino <dhandelfino@gmail.com>
2026-03-09 23:59:30 -0300

    feat: alterando controller para receber rotas

--- src/main/java/com/laundry/adapters/input/controllers/OrderController.java
+++ src/main/java/com/laundry/adapters/input/controllers/OrderController.java
@@ -1,21 +1,43 @@
 package com.laundry.adapters.input.controllers;
 
 import com.laundry.application.usecases.CreateOrderUsecase;
+import com.laundry.application.usecases.FindOrderByClientUsecase;
+import com.laundry.application.usecases.ListOrderUsecase;
 import com.laundry.domain.entity.Order;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pedidos")
 public class OrderController {
 
-    private final CreateOrderUsecase createOrderUsecase;
+    private final CreateOrderUsecase criarPedido;
+    private final ListOrderUsecase listarPedidos;
+    private final FindOrderByClientUsecase buscarPorCliente;
 
-    public OrderController(CreateOrderUsecase createOrderUsecase) {
-        this.createOrderUsecase = createOrderUsecase;
+    public OrderController(
+            CreateOrderUsecase criarPedido,
+            ListOrderUsecase listarPedidos,
+            FindOrderByClientUsecase buscarPorCliente) {
+
+        this.criarPedido = criarPedido;
+        this.listarPedidos = listarPedidos;
+        this.buscarPorCliente = buscarPorCliente;
     }
 
     @PostMapping
-    public Order createOrder(@RequestBody Order order) {
-        return createOrderUsecase.executar(order);
+    public Order criar(@RequestBody Order order) {
+        return criarPedido.executar(order);
+    }
+
+    @GetMapping
+    public List<Order> listar() {
+        return listarPedidos.executar();
+    }
+
+    @GetMapping("/cliente/{cpfCliente}")
+    public List<Order> buscarPorCliente(@PathVariable String cpfCliente) {
+        return buscarPorCliente.executar(cpfCliente);
     }
 }
