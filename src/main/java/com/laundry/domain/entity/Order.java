024ba5d85c4d34ad588af11e4e4da5e9b0ae93c7
Danielle Delfino <dhandelfino@gmail.com>
2026-03-09 23:59:21 -0300

    feat: alterando controller para receber rotas

--- src/main/java/com/laundry/domain/entity/Order.java
+++ src/main/java/com/laundry/domain/entity/Order.java
@@ -1,6 +1,7 @@
 package com.laundry.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ public class Order {
 
     private String nomeCliente;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String cpfCliente;
 
     private String cepCliente;
