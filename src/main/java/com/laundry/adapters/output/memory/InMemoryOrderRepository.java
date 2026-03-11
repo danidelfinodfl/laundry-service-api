804bd76171f127b8be18b847eb8b809f72c4b67e
Danielle Delfino <dhandelfino@gmail.com>
2026-03-10 22:32:38 -0300

    feat: comentando repository para não conflitar com dynamo

--- src/main/java/com/laundry/adapters/output/memory/InMemoryOrderRepository.java
+++ src/main/java/com/laundry/adapters/output/memory/InMemoryOrderRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
+//@Repository
 public class InMemoryOrderRepository implements OrderRepository {
 
     private final Map<String, Order> banco = new HashMap<>();
